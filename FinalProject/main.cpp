75a47c16bdf8217fc1d062f18f5150667cbf48a5
Adam T. Barson <adam.barson@uvm.edu>
2017-04-13 10:56:05 -0400

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Adam Barson on 4/13/17.
+//  Copyright © 2017 Adam Barson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
